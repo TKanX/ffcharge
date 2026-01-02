28a51af4a628e919b033afe96f4ed5785980b371
Tony Kan <tianchengkan@gmail.com>
2026-01-02 15:49:36 -0800

    feat(scheme): Export charge scheme module and export schemes

---
+++ src/scheme/mod.rs
@@ -0,0 +1,13 @@
+//! Charge scheme definitions.
+
+mod dna;
+mod ion;
+mod protein;
+mod rna;
+mod water;
+
+pub use dna::DnaScheme;
+pub use ion::IonScheme;
+pub use protein::ProteinScheme;
+pub use rna::RnaScheme;
+pub use water::WaterScheme;
