c85e7569ed2a237e396f7eabf465f25fb47d44f5
Tony Kan <tianchengkan@gmail.com>
2026-01-03 23:01:47 -0800

    refactor(scheme): Simplify RnaScheme by consolidating variants and updating documentation

--- src/scheme/rna.rs
+++ src/scheme/rna.rs
@@ -3,28 +3,25 @@
 /// RNA charge scheme.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
 pub enum RnaScheme {
-    /// AMBER OL3.
+    /// AMBER (AMBER OL3).
     #[default]
-    AmberOL3,
-    /// CHARMM36.
-    CharmmC36,
-    /// CHARMM27.
-    CharmmC27,
+    Amber,
+    /// CHARMM (CHARMM C27, C36).
+    Charmm,
 }
 
 impl RnaScheme {
     /// Returns the key for this scheme.
     pub const fn key(self) -> &'static str {
         match self {
-            Self::AmberOL3 => "amber-ol3",
-            Self::CharmmC36 => "charmm-c36",
-            Self::CharmmC27 => "charmm-c27",
+            Self::Amber => "amber",
+            Self::Charmm => "charmm",
         }
     }
 
     /// Returns all available schemes.
     pub const fn all() -> &'static [Self] {
-        &[Self::AmberOL3, Self::CharmmC36, Self::CharmmC27]
+        &[Self::Amber, Self::Charmm]
     }
 }
 
