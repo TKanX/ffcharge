cac00cf7ad361a123da9f186a00fd1907f21ecb2
Tony Kan <tianchengkan@gmail.com>
2026-01-04 18:44:41 -0800

    feat(lib): Define WaterCharges struct for water charge distribution

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,14 @@ mod scheme;
 
 pub use position::Position;
 pub use scheme::{DnaScheme, IonScheme, ProteinScheme, RnaScheme, WaterScheme};
+
+/// Water charge distribution.
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct WaterCharges {
+    /// Charge on Oxygen.
+    pub o: f32,
+    /// Charge on Hydrogen 1.
+    pub h1: f32,
+    /// Charge on Hydrogen 2.
+    pub h2: f32,
+}
